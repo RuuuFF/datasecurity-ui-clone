2027f4b463e1b621092e7776385f20b912a00675
Carlos Daniel <dev.cdfaria@gmail.com>
2021-04-15 00:52:04 -0300

    h1 and h2 formatter

--- assets/scripts/Fonts.js
+++ assets/scripts/Fonts.js
@@ -1,10 +1,26 @@
 const Fonts = [
   {
-    el: document.querySelectorAll(".logo h1"),
+    el: document.querySelectorAll(".logo a"),
     minFontsize: 1.8,
     maxFontsize: 2.8,
     minLineheight: 2,
     maxLineheight: 3
+  },
+
+  {
+    el: document.querySelectorAll("h1"),
+    minFontsize: 2,
+    maxFontsize: 3,
+    minLineheight: 2.6,
+    maxLineheight: 3.6
+  },
+
+  {
+    el: document.querySelectorAll("h2"),
+    minFontsize: 4.8,
+    maxFontsize: 5.2,
+    minLineheight: 5.68,
+    maxLineheight: 5.28
   },
 
   {
