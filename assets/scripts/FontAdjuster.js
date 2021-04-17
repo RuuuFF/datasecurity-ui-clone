c9b3f0a91ecc7959a55d10794e8ba8f0ec38e8b3
Carlos Daniel <dev.cdfaria@gmail.com>
2021-04-17 16:49:10 -0300

    Getting options and sources

--- assets/scripts/FontAdjuster.js
+++ assets/scripts/FontAdjuster.js
@@ -1,6 +1,60 @@
-import { Fonts, Options } from './Fonts.js'
+const Options = {
+  minWidth: 420,
+  maxWidth: 1365,
+  measure: "rem"
+}
 
-const Font = {
+const Fonts = [
+  {
+    selector: ".logo a",
+    minFontsize: 1.8,
+    maxFontsize: 2.8,
+    minLineheight: 2,
+    maxLineheight: 3
+  },
+
+  {
+    selector: "h1",
+    minFontsize: 2,
+    maxFontsize: 3,
+    minLineheight: 2.6,
+    maxLineheight: 3.6
+  },
+
+  {
+    selector: "h2",
+    minFontsize: 4.8,
+    maxFontsize: 5.2,
+    minLineheight: 5.68,
+    maxLineheight: 5.28
+  },
+
+  {
+    selector: "h3",
+    minFontsize: 2,
+    maxFontsize: 3,
+    minLineheight: 2.6,
+    maxLineheight: 3.6
+  },
+
+  {
+    selector: "h4",
+    minFontsize: 2,
+    maxFontsize: 2.4,
+    minLineheight: 2.6,
+    maxLineheight: 3
+  },
+
+  {
+    selector: "p",
+    minFontsize: 1.4,
+    maxFontsize: 1.6,
+    minLineheight: 2.6,
+    maxLineheight: 2.8
+  }
+]
+
+const FontAdjust = {
   scale(num, in_min, in_max, out_min, out_max) {
     let value = (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
     
@@ -13,23 +67,36 @@ const Font = {
   scaler(min, max) {
     const screenWidth = Number(window.innerWidth)
 
-    return Font.scale(screenWidth, Number(Options.minWidth), Number(Options.maxWidth), min, max)
+    return FontAdjust.scale(screenWidth, Number(Options.minWidth), Number(Options.maxWidth), min, max)
   },
 
-  fontAdjust() {
-    Fonts.forEach(obj => {
+  createStyleEl() {
+    const head = document.getElementsByTagName('head')[0]
+    const style = document.createElement('style')
+    head.appendChild(style)
+  },
+
+  adjustSize() {
+    const style = document.getElementsByTagName('style')[0]
+
+    style.innerHTML = "/* Style injected by FontAdjust ^^ */"
+
+    Fonts.forEach((obj) => {
       const { selector, minFontsize, maxFontsize, minLineheight, maxLineheight } = obj
 
-      document.querySelectorAll(selector).forEach(el => {
-        const fontSize = Font.scaler(minFontsize, maxFontsize)
-        const lineHeight = Font.scaler(minLineheight, maxLineheight)
+      const fontSize = FontAdjust.scaler(minFontsize, maxFontsize).toFixed(1)
+      const lineHeight = FontAdjust.scaler(minLineheight, maxLineheight).toFixed(1)
 
-        el.style.fontSize = String(fontSize + Options.measure)
-        el.style.lineHeight = String(lineHeight + Options.measure)
-      })
+      style.innerHTML += `
+      ${selector} {
+        font-size: ${String(fontSize + Options.measure)};
+        line-height: ${String(lineHeight + Options.measure)};
+      }
+      `
     })
   },
 }
 
-window.addEventListener('resize', Font.fontAdjust)
-Font.fontAdjust()
+FontAdjust.createStyleEl()
+FontAdjust.adjustSize()
+window.addEventListener('resize', FontAdjust.adjustSize)
