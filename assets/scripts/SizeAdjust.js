dea9b68dbed424e56226c20e559da5d37948bdf3
Carlos Daniel <dev.cdfaria@gmail.com>
2021-04-18 23:00:34 -0300

    Refactored

--- assets/scripts/SizeAdjust.js
+++ assets/scripts/SizeAdjust.js
@@ -8,133 +8,94 @@ CSSSelectors = [
   {
     selector: ".logo a",
     propAndValue: [
-      {
-        property: "font-size",
-        min: 1.8,
-        max: 2.8
-      },
-      {
-        property: "line-height",
-        min: 2,
-        max: 3,
-      }
+      { property: "font-size", min: 1.8, max: 2.8 },
+      { property: "line-height", min: 2, max: 3 }
     ]
   },
 
   {
     selector: "h1",
     propAndValue: [
-      {
-        property: "font-size",
-        min: 2.,
-        max: 3,
-      },
-      {
-        property: "line-height",
-        min: 2.6,
-        max: 3.6,
-      }
+      { property: "font-size", min: 2, max: 3 },
+      { property: "line-height", min: 2.6, max: 3.6 }
     ]
   },
 
   {
     selector: "h2",
     propAndValue: [
-      {
-        property: "font-size",
-        min: 2.8,
-        max: 5.2,
-      },
-      {
-        property: "line-height",
-        min: 5.6,
-        max: 5.2,
-      }
+      { property: "font-size", min: 2.8, max: 5.2 },
+      { property: "line-height", min: 5.6, max: 5.2 }
     ]
   },
 
   {
     selector: "h2.bigger",
     propAndValue: [
-      {
-        property: "font-size",
-        min: 7.2,
-        max: 7.2,
-      },
-      {
-        property: "line-height",
-        min: 7.9,
-        max: 7.9,
-      }
+      { property: "font-size", min: 7.2, max: 7.2 },
+      { property: "line-height", min: 7.9, max: 7.9 }
     ]
   },
 
   {
     selector: "h3",
     propAndValue: [
-      {
-        property: "font-size",
-        min: 2.4,
-        max: 2.4,
-      },
-      {
-        property: "line-height",
-        min: 2.8,
-        max: 2.8,
-      }
+      { property: "font-size", min: 2.4, max: 2.4 },
+      { property: "line-height", min: 2.8, max: 2.8 }
     ]
   },
 
   {
     selector: "h3.bigger",
     propAndValue: [
-      {
-        property: "font-size",
-        min: 4,
-        max: 4,
-      },
-      {
-        property: "line-height",
-        min: 4.8,
-        max: 4.8,
-      }
+      { property: "font-size", min: 4, max: 4 },
+      { property: "line-height", min: 4.8, max: 4.8 }
     ]
   },
 
   {
     selector: "h4",
     propAndValue: [
-      {
-        property: "font-size",
-        min: 2,
-        max: 2.4,
-      },
-      {
-        property: "line-height",
-        min: 2.6,
-        max: 3,
-      }
+      { property: "font-size", min: 2, max: 2.4 },
+      { property: "line-height", min: 2.6, max: 3 }
     ]
   },
 
   {
     selector: "p",
     propAndValue: [
-      {
-        property: "font-size",
-        min: 1.4,
-        max: 1.6,
-      },
-      {
-        property: "line-height",
-        min: 2.6,
-        max: 2.8,
-      }
+      { property: "font-size", min: 1.4, max: 1.6 },
+      { property: "line-height", min: 2.6, max: 2.8 }
     ]
   },
 
   {
-    selector: ".third-section section:last-child ol li a",
+    selector: "p.bigger",
+    propAndValue: [
+      { property: "font-size", min: 2, max: 2 },
+      { property: "line-height", min: 3.6, max:3.6 }
+    ]
+  },
+
+  {
+    selector: ".first-section .image-container::after",
+    propAndValue: [
+      { property: "width", min: 7.4, max: 20.4 },
+      { property: "height", min: 7.4, max: 20.4 },
+      { property: "top", min: 2, max: 5 }
+    ]
+  },
+  
+  {
+    selector: ".third-section section:first-child .image-container",
+    propAndValue: [
+      { property: "width", min: 32, max: 73 },
+      { property: "height", min: 23, max: 41 }
+    ]
+  },
+
+  {
+    selector: ".third-section section:last-child ol a",
     propAndValue: [
       {
         property: "font-size",
